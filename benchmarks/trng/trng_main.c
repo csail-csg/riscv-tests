19124e861f545b778355a79a77a2509af0a23788
Sizhuo Zhang <szzhang@mit.edu>
2018-05-17 14:51:49 -0400

    make benchmarks tohost/fromhost addr same as linux bbl

--- benchmarks/trng/trng_main.c
+++ benchmarks/trng/trng_main.c
@@ -8,7 +8,7 @@ uint64_t random_byte() {
 int main( int argc, char* argv[] ) {
     printf("Hi! Here are 16 readouts from the TRNG:\n");
     for (int i=0; i<16; i++) {
-        printf("  %d\n", random_byte());
+        printf("  %016llx\n", (long long unsigned)(random_byte()));
     }
     return 0;
 }
